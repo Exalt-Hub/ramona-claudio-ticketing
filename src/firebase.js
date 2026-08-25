8db17eef771d9ced226259443980277264f3e7ca
Exalt-Hub <ajesuriname2@gmail.com>
2026-08-25 13:05:15 -0300

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,5 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+const firebaseConfig={apiKey:'AIzaSyDS-vjnRiGnSbLrUpAqNm-7ytlsDzXm5hE',authDomain:'glassy-acolyte-472202-h0.firebaseapp.com',projectId:'glassy-acolyte-472202-h0',storageBucket:'glassy-acolyte-472202-h0.firebasestorage.app',messagingSenderId:'1032418782692',appId:'1:1032418782692:web:9a265435183409ee8aabf1'};
+const app=initializeApp(firebaseConfig); export const db=getFirestore(app); export const auth=getAuth(app);
